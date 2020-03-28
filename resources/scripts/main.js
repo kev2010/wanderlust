b1510f36def5e7e2ae88cb3bab31db290696edff
Kevin Jiang <kev2018@mit.edu>
2020-03-27 22:21:02 -0400

    Add Foursquaure API Call

---
+++ resources/scripts/main.js
@@ -0,0 +1,70 @@
+//  Foursquare API Info
+const clientId = 'GVGPRMLDJEQWI40B2AWXSRMU34T3SNZ3YT42KLWG0JNPTDXE';
+const clientSecret = 'GOLVUO32TKPHUETFGQXWS2ECG0YKBVYV4VU3G2EIMKUZ3C3C';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';
+
+//  OpenWeather Info
+const openWeatherKey = 'b491b5935742f0ceae6e4363df57cb59';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Page Elements
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
+const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
+const $weatherDiv = $("#weather1");
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// Add AJAX functions here:
+const getVenues = async () => {
+    const city = $input.val();
+    const urlToFetch = url + city + '&limit=10&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20200327';
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+            console.log(venues);
+            return venues;
+        }
+        throw new Error('Request failed!');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getForecast = () => {
+
+}
+
+
+// Render functions
+const renderVenues = (venues) => {
+  $venueDivs.forEach(($venue, index) => {
+    // Add your code here:
+
+    let venueContent = '';
+    $venue.append(venueContent);
+  });
+  $destination.append(`<h2>${venues[0].location.city}</h2>`);
+}
+
+const renderForecast = (day) => {
+  // Add your code here:
+  
+	let weatherContent = '';
+  $weatherDiv.append(weatherContent);
+}
+
+const executeSearch = () => {
+  $venueDivs.forEach(venue => venue.empty());
+  $weatherDiv.empty();
+  $destination.empty();
+  $container.css("visibility", "visible");
+  getVenues()
+  getForecast()
+  return false;
+}
+
+$submit.click(executeSearch)
