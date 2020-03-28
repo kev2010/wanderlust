42e4008a876d147ef55d634830f042568cf803f3
Kevin Jiang <kev2018@mit.edu>
2020-03-27 22:27:41 -0400

    Add OpenWeather API Call

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -34,8 +34,18 @@ const getVenues = async () => {
     }
 }
 
-const getForecast = () => {
-
+const getForecast = async () => {
+    const urlToFetch = weatherUrl + '?q=' + $input.val() + '&APPID=' + openWeatherKey;
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            return jsonResponse;
+        }
+        throw new Error('Request failed!')
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
